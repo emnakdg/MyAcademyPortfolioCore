d8ef13b88003f4ea9f353edfe933b668f0edb45c
Emin Akdag <emin.akdag20@gmail.com>
2025-08-07 22:22:23 +0300

    admin template tamamlandı,  adminlayout ve category controller eklendi, ilgili controllerların viewsları eklendi

--- Portfolio.Web/Program.cs
+++ Portfolio.Web/Program.cs
@@ -1,6 +1,10 @@
+using Portfolio.Web.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//IOC Container
+builder.Services.AddScoped<PortfolioContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
