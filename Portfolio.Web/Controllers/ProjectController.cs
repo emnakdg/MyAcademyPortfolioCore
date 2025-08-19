35a842f44395ed87f1099f1efd037b799074af12
Emin Akdag <emin.akdag20@gmail.com>
2025-08-19 22:22:52 +0300

    proje ana sayfası eklendi.

--- Portfolio.Web/Controllers/ProjectController.cs
+++ Portfolio.Web/Controllers/ProjectController.cs
@@ -46,5 +46,33 @@ namespace Portfolio.Web.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult UpdateProject(int id)
+        {
+            CategoryDropdown();
+            var project = context.Projects.Find(id);
+            return View(project);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProject(Project project)
+        {
+            CategoryDropdown();
+            if (!ModelState.IsValid)
+            {
+                return View(project);
+            }
+            context.Projects.Update(project);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProject(int id)
+        {
+            var project = context.Projects.Find(id);
+            context.Projects.Remove(project);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
