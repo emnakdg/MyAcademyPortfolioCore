ee9c9434921e7b54aa046de83137262800a1da47
Emin Akdag <emin.akdag20@gmail.com>
2025-08-26 22:05:21 +0300

    İstatistik controller ve işlemleri eklendi.

---
+++ Portfolio.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Context;
+
+namespace Portfolio.Web.Controllers
+{
+    public class StatisticsController(PortfolioContext context) : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.projectCount = context.Projects.Count();
+            ViewBag.SkillAverage = context.Skills.Any() ? context.Skills.Average(x => x.Percentage).ToString("00.00") : 0.0.ToString("00.00");
+            ViewBag.UnreadMessageCount = context.UserMessages.Count(x => x.IsRead == false);
+            ViewBag.LastMessageOwner = context.UserMessages.OrderByDescending(x => x.UserMessageId).Select(x => x.Name).FirstOrDefault();
+            var startYear = context.Experiences.Min(x => x.StartYear);
+            ViewBag.ExperienceYear = DateTime.Now.Year - startYear;
+            ViewBag.CompanyCount = context.Experiences.Select(x => x.Company).Distinct().Count();
+            ViewBag.ReviewAverage = context.Testimonials.Any() ? context.Testimonials.Average(x => x.Review).ToString("0.0") : "Değerlendirme Yapılmadı";
+            ViewBag.MaxReviewOwner = context.Testimonials.OrderByDescending(x => x.Review).Select(x => x.Name).FirstOrDefault();
+            return View();
+        }
+    }
+}
